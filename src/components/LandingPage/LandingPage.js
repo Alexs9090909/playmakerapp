d50a31eac1ffd961ca4fd3774f6102e4fe7eab2b
Alexs9090909 <alexsutocode@gmail.com>
2023-12-01 19:38:01 +0100

    hashrouter 01

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,12 +1,15 @@
 // LandingPage.js
 import React from 'react';
-import styles from './LandingPage.module.css'; // Ajoutez des styles si nécessaire
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import styles from './LandingPage.module.css';
+import { Link } from 'react-router-dom';
 import { getToken } from '../../utils/Spotify';
 import logo from '../../logo.svg';
 
 const LandingPage = () => {
+  const handleConnectClick = () => {
+    getToken();
+  };
+
   return (
     <div className={styles.LandingPage}>
       <div className={styles.TextContent}>
@@ -14,14 +17,16 @@ const LandingPage = () => {
         <p>Thanks for checking my Portfolio! &#x1F60A; </p>
         <p>Unfortunately <strong>Spotify</strong> has now limited the number of users who can access their API &#x1F611;</p>
         <p>If you want me to add you to the list, just <a href="mailto:alexsutocode@gmail.com">send me an email</a>, and I’ll be happy to do so:</p>
-        <p>Otherwise if you’re just wandering around, you can just use these logins and have a go at it :</p>
+        <p>Otherwise if you’re just wandering around, you can just use these logins and have a go at it:</p>
         <p className={styles.Login}>Email: playmakerapp00@gmail.com<br></br>
-        Password: Playmaker1234</p>
+          Password: Playmaker1234</p>
       </div>
-      <button className={styles.ConnectButton} onClick={getToken}>
-        <img src={logo} className="App-logo" alt="logo" />
-        Connect
-      </button>
+      <Link to="/App">
+        <button className={styles.ConnectButton} onClick={handleConnectClick}>
+          <img src={logo} className="App-logo" alt="logo" />
+          Connect
+        </button>
+      </Link>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Remplace BrowserRouter par HashRouter
 import LandingPage from './components/LandingPage/LandingPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,7 +18,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
